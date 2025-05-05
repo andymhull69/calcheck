40ae9384bce66e7d551a26ad74c6758017429d07
andymhull69 <andrew@fouracres.net>
2025-05-05 21:59:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def web_output():
     <head>
         <title>CH Sports Rehab – Bramhall Clinic</title>
         <style>
-            body { font-family: Arial; padding: 1px; max-width: 1000px; margin: 0 5px; }
+            body { font-family: Arial; padding: 1px; max-width: 1200px; margin: 0 5px; }
             .mobile-banner { display: none; }
             h1 { color: #2c3e50; }
             .days-grid {

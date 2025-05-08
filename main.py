507b859946aeda882dc543ca122bd6a0501cdbca
andymhull69 <andrew@fouracres.net>
2025-05-08 18:50:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -323,9 +323,9 @@ def show_responses():
         timestamp = row.get("Timestamp", "Unknown date/time")
         name = row.get("Full name", "Unknown name")
         other_details = "".join(
-            f"<li><strong>{k}:</strong> {v}</li>
-            for k, v in row.items() if k not in ['Timestamp', 'Full name']
-        )
+        f"<li><strong>{k}:</strong> {v}</li>"
+        for k, v in row.items() if k not in ['Timestamp', 'Full name']
+    )
         session_details = row.get("session details", "")
         html += f"""
         <li>
